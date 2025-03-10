7337d6c5241a209d80216121d6e9a29b3cc3aa79
wuyong <biggodw@gmail.com>
2025-03-10 23:51:46 +0800

    返回

--- components/base/backshare.vue
+++ components/base/backshare.vue
@@ -1,27 +1,35 @@
 <template>
-    <div class="flex justify-between items-center sticky h-12 px-2 bg-slate-50 top-0 z-50" >
+    <div class="flex justify-between items-center sticky h-12 px-2 bg-slate-50 top-0 z-50">
         <div class="flex justify-center items-center ">
             <BaseIcomfont iconname="icon-fanhui" myclass="text-2xl"></BaseIcomfont>
-            <span @click="$router.back()">返回</span>
+            <span @click="back()">返回</span>
         </div>
         <div class="flex justify-center items-center " @click="share()">
             <BaseIcomfont iconname="icon-response" myclass="text-2xl"></BaseIcomfont>
         </div>
-        
+
     </div>
 </template>
 <script setup>
 const router = useRouter()
-const share = ()=>{
+const share = () => {
     console.log('share')
-    if(navigator?.share){
+    if (navigator?.share) {
         navigator.share({
-            title:document.title,
-            url:document.querySelector('link[rel=canonical]') ?
-  document.querySelector('link[rel=canonical]').href :
-  document.location.href,
-            text:document.title
+            title: document.title,
+            url: document.querySelector('link[rel=canonical]') ?
+                document.querySelector('link[rel=canonical]').href :
+                document.location.href,
+            text: document.title
         })
     }
+}
+const back = ()=>{
+    if (window.history.length > 1) {
+    router.go(-1);
+  } else {
+    // 处理首页跳转失败的情况
+    window.location.href = '/'; // 强制跳转
+  }
 }
 </script>
