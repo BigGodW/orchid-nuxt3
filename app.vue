2b741e7684ee6e7630d6c174d27edd0a453c8c69
wuyong <biggodw@gmail.com>
2025-03-11 00:39:23 +0800

    baidu

--- app.vue
+++ app.vue
@@ -15,7 +15,8 @@ useHead({
     script: [{ src: "https://at.alicdn.com/t/c/font_4842723_51f9txm4sx2.js" }],
     title:'中国兰花博览园官网',
     meta: [
-    { name: 'description', content: '兰花 中国兰花 国兰 春兰 春剑 莲瓣兰 蕙兰' }
+    { name: 'description', content: '兰花 中国兰花 国兰 春兰 春剑 莲瓣兰 蕙兰' },
+    { name: 'baidu-site-verification', content: 'codeva-FIVbQWDHKR' },
   ],
 
 })
